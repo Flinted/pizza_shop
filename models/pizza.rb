b83d65beb23cfeb5f910e4fcf13e88606a3bed06
Chris Flint <flintedc@gmail.com>
2016-06-21 11:23:20 +0100

    added mapping function

--- models/pizza.rb
+++ models/pizza.rb
@@ -8,7 +8,7 @@ class Pizza
     def initialize(options)
       @first_name = options["first_name"]
       @last_name  = options["last_name"]
-      @topping    = options["topping"]
+      @topping    = options["toppings"]
       @quantity   = options["quantity"].to_i
     end
 
@@ -18,7 +18,7 @@ class Pizza
         pizzas (
         first_name,
         last_name,
-        topping,
+        toppings,
         quantity) 
         VALUES (
         '#{@first_name}',
@@ -29,11 +29,26 @@ class Pizza
       db.exec(sql)
       db.close
     end
+
+    def self.all()
+      db =  PG.connect({dbname:'pizza_shop', host: 'localhost'})
+      sql = "SELECT * FROM pizzas"
+      pizzas = db.exec(sql)
+      db.close
+      # pizza_array = []
+      # for pizza in pizzas
+      #   pizza_array << Pizza.new(pizza)
+      # end
+      # puts pizza_array
+      
+      pizza_array = pizzas.map {|pizza| Pizza.new(pizza)}
+      puts pizza_array
+    end
 end
 
 pizza1 = Pizza.new({"first_name" => "Obiwan", "last_name" => "Kenobe", "topping" => "Napoli", "quantity" => 1})
 pizza2 = Pizza.new({"first_name" => "Luke", "last_name" => "Skywalker", "topping" => "Calzone", "quantity" => 5})
 
-pizza1.save()
+Pizza.all()
 binding.pry
 nil
